719f7c9686d85d5a0e0491858f18f0045b3563ed
Mukesh Shankar <64324363+MukeshCSK@users.noreply.github.com>
2023-08-08 18:23:54 +0530

    Add files via upload

---
+++ Python Programs/Remove the Background from an image.py
@@ -0,0 +1,24 @@
+#pip install rembg
+#pip install Pillow
+
+
+# Importing Required Modules
+from rembg import remove
+from PIL import Image
+
+# Store path of the image in the variable input_path
+input_path = "C:\\Users\\mukes\\Desktop\\Mukesh.jpg"
+
+# Store path of the output image in the variable output_path
+output_path = "C:\\Users\\mukes\\Desktop\\Output.png"
+
+# Processing the image
+input = Image.open(input_path)
+
+# Removing the background from the given Image
+output = remove(input)
+
+#Saving the image in the given path
+output.save(output_path)
+
+print("Mission Done")
